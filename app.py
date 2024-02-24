bae43fcf9030a094e33baf52d26ddb1b59e9a92f
Muhammad Zubair Yousafzai <75239683+Muhammad-Zubair-Yousafzai@users.noreply.github.com>
2024-02-24 23:35:08 +0500

    app.py

--- app.py
+++ app.py
@@ -36,18 +36,21 @@ if uploaded_file is not None:
     ax = fig.add_subplot(111, projection='3d')
     
     # Create a grid of x, y, z coordinates
-    x, y = np.meshgrid(np.arange(img_array.shape[1]), np.arange(img_array.shape[0]))
+    x, y = np.meshgrid(np.arange(image.size[1]), np.arange(image.size[0]))
     r = img_array[:, :, 0]
     g = img_array[:, :, 1]
     b = img_array[:, :, 2]
     
     # Plot the surface for red channel
     ax.plot_surface(x, y, r, cmap='Reds', linewidth=0)
+    ax.set_xlim(0, image.size[1])
+    ax.set_ylim(0, image.size[0])
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Red')
-
+    
     st.pyplot(fig)
+
     # Get predictions
     img_batch = np.expand_dims(image, 0)
     predictions = MODEL.predict(img_batch)

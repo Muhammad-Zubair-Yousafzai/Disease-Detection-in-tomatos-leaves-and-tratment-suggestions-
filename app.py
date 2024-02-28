3c67770819aab67d0fea6a0d459e6c6e3058d83a
Muhammad Zubair Yousafzai <75239683+Muhammad-Zubair-Yousafzai@users.noreply.github.com>
2024-02-28 18:40:29 +0500

    app.py

--- app.py
+++ app.py
@@ -24,29 +24,21 @@ CLASS_NAMES = ['Tomato_Bacterial_spot',
  'Tomato_healthy']
 
 
-
 # Function to display medicine recommendations
 def display_medicine(predicted_class):
-    if predicted_class == 'Tomato_Bacterial_spot':
-        st.info('A plant with bacterial spot cannot be cured. Remove symptomatic plants from the field or greenhouse to prevent the spread of bacteria to healthy plants. Burn, bury or hot compost the affected plants and DO NOT eat symptomatic fruit.')
-    elif predicted_class == 'Tomato_Early_blight':
-        st.info('Cure the plant quickly otherwise the disease can be spread, Thoroughly spray the plant (bottoms of leaves also) with Bonide Liquid Copper Fungicide concentrate or Bonide Tomato & Vegetable')
-    elif predicted_class == 'Tomato_Late_blight':
-        st.info('Spraying fungicides is the most effective way to prevent late blight. For conventional gardeners and commercial producers, protectant fungicides such as chlorothalonil (e.g., Bravo, Echo, Equus, or Daconil) and Mancozeb (Manzate) can be used.')
-    elif predicted_class == 'Tomato_Leaf_Mold':
-        st.info('Baking soda solution: Mix 1 tablespoon baking soda and ½ teaspoon liquid soap such as Castile soap (not detergent) in 1 gallon of water. Spray liberally, getting top and bottom leaf surfaces and any affected areas.')
-    elif predicted_class == 'Tomato_Septoria_leaf_spot':
-        st.info('Fungicides with active ingredients such as chlorothalonil, copper, or mancozeb will help reduce disease, but they must be applied before disease occurs as they can only provide preventative protection. They will not cure the plant. If the disease has spread then remove the plants')
-    elif predicted_class == 'Tomato_Spider_mites_Two_spotted_spider_mite':
-        st.info('Aiming a hard stream of water at infested plants to knock spider mites off the plants. Other options include insecticidal soaps, horticultural oils, or neem oil.')
-    elif predicted_class == 'Tomato__Target_Spot':
-        st.info('Products containing chlorothalonil, mancozeb, and copper oxychloride have been shown to provide good control of target spot in research trials.')
-    elif predicted_class == 'Tomato__Tomato_YellowLeaf__Curl_Virus':
-        st.info('Use a neonicotinoid insecticide, such as dinotefuran (Venom) imidacloprid (AdmirePro, Alias, Nuprid, Widow, and others) or thiamethoxam (Platinum), as a soil application or through the drip irrigation system at transplanting of tomatoes or peppers.')
-    elif predicted_class == 'Tomato__Tomato_mosaic_virus':
-        st.info('Remove all infected plants and destroy them. Do NOT put them in the compost pile, as the virus may persist in infected plant matter. Monitor the rest of your plants closely, especially those that were located near infected plants. Disinfect gardening tools after every use.')
-    elif predicted_class == 'Tomato_healthy':
-        st.info('Your plant is healthy, there is no need to apply medicines, please take care of your plants, if any disease occurs, then cure it fast and remove the infected leaves.')
+    recommendations = {
+        'Tomato_Bacterial_spot': 'A plant with bacterial spot cannot be cured. Remove symptomatic plants from the field or greenhouse to prevent the spread of bacteria to healthy plants. Burn, bury, or hot compost the affected plants and DO NOT eat symptomatic fruit.',
+        'Tomato_Early_blight': 'Cure the plant quickly otherwise the disease can be spread, Thoroughly spray the plant (bottoms of leaves also) with Bonide Liquid Copper Fungicide concentrate or Bonide Tomato & Vegetable',
+        'Tomato_Late_blight': 'Spraying fungicides is the most effective way to prevent late blight. For conventional gardeners and commercial producers, protectant fungicides such as chlorothalonil (e.g., Bravo, Echo, Equus, or Daconil) and Mancozeb (Manzate) can be used.',
+        'Tomato_Leaf_Mold': 'Baking soda solution: Mix 1 tablespoon baking soda and ½ teaspoon liquid soap such as Castile soap (not detergent) in 1 gallon of water. Spray liberally, getting top and bottom leaf surfaces and any affected areas.',
+        'Tomato_Septoria_leaf_spot': 'Fungicides with active ingredients such as chlorothalonil, copper, or mancozeb will help reduce disease, but they must be applied before disease occurs as they can only provide preventative protection. They will not cure the plant. If the disease has spread then remove the plants',
+        'Tomato_Spider_mites_Two_spotted_spider_mite': 'Aiming a hard stream of water at infested plants to knock spider mites off the plants. Other options include insecticidal soaps, horticultural oils, or neem oil.',
+        'Tomato__Target_Spot': 'Products containing chlorothalonil, mancozeb, and copper oxychloride have been shown to provide good control of target spot in research trials.',
+        'Tomato__Tomato_YellowLeaf__Curl_Virus': 'Use a neonicotinoid insecticide, such as dinotefuran (Venom) imidacloprid (AdmirePro, Alias, Nuprid, Widow, and others) or thiamethoxam (Platinum), as a soil application or through the drip irrigation system at transplanting of tomatoes or peppers.',
+        'Tomato__Tomato_mosaic_virus': 'Remove all infected plants and destroy them. Do NOT put them in the compost pile, as the virus may persist in infected plant matter. Monitor the rest of your plants closely, especially those that were located near infected plants. Disinfect gardening tools after every use.',
+        'Tomato_healthy': 'Your plant is healthy, there is no need to apply medicines, please take care of your plants, if any disease occurs, then cure it fast and remove the infected leaves.'
+    }
+    st.info(recommendations.get(predicted_class, "No recommendation available for this class."))
 
 
 # Function to generate heatmap
@@ -54,45 +46,17 @@ def generate_heatmap(image, disease_mask):
     # Apply colormap to disease mask
     cmap = LinearSegmentedColormap.from_list('custom', [(0, 'green'), (1, 'red')])
     disease_heatmap = cmap(disease_mask)
-    
-    # Overlay heatmap on image
-    overlaid_image = Image.fromarray((image * 255).astype(np.uint8))
-    overlaid_image.putalpha(0)  # Set opacity to 50%
-    overlaid_image = overlaid_image.convert("RGB")
-    
-    # Set figure size
-    plt.figure(figsize=(width, height))
-    
-    plt.imshow(overlaid_image)
-    plt.imshow(disease_heatmap, alpha=0.5)
-    plt.axis('off')
-    return plt.gcf()
 
-# Function to generate accuracy plot
-def generate_accuracy_plot(epochs, accuracy):
-    plt.figure(figsize=(8, 6))
-    plt.plot(epochs, accuracy, label='Accuracy')
-    plt.title('Accuracy Over Epochs')
-    plt.xlabel('Epochs')
-    plt.ylabel('Accuracy')
-    plt.legend()
-    return plt.gcf()
-
-# Function to generate heatmap
-def generate_heatmap(image, disease_mask):
-    # Apply colormap to disease mask
-    cmap = LinearSegmentedColormap.from_list('custom', [(0, 'green'), (1, 'red')])
-    disease_heatmap = cmap(disease_mask)
-    
     # Overlay heatmap on image
     overlaid_image = Image.fromarray((image * 255).astype(np.uint8))
     overlaid_image.putalpha(128)  # Set opacity to 50%
     overlaid_image = overlaid_image.convert("RGB")
-    
+
     plt.imshow(overlaid_image)
     plt.imshow(disease_heatmap, alpha=0.5)
     plt.axis('off')
     return plt.gcf()
+
 
 # Function to predict disease and generate heatmap
 def predict_disease_and_generate_heatmap(image):
@@ -125,8 +89,9 @@ def predict_disease_and_generate_heatmap(image):
     fig = generate_heatmap(img_array, disease_mask)
     st.pyplot(fig)
 
+
 # Main code
-uploaded_file = st.file_uploader("Choose an image ...", type="jpg")
+uploaded_file = st.file_uploader("Choose an image ...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
